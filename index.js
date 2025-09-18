60db4c51e352c2d09be38b6c5e12cbd099dcb395
MatiCABJ12 <50233785@est.ort.edu.ar>
2025-09-18 10:22:20 -0300

    1° Wally

---
+++ index.js
@@ -0,0 +1,7 @@
+import {generarEspacio} from "./busqueda.js";
+
+ let array =generarEspacio(100)
+ 
+for(let i =0;i <array.length;i++){
+if (array [i]=== "Wally"){
+    console.log (i+") " + array[i])}}
